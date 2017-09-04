414289b78ac621bae7efe73dda5c91eb4a608e04
LXZ_svn_sync <lixizhi@yeah.net>
2017-09-04 10:07:34 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/Tasks/TransformWnd.lua
+++ script/apps/Aries/Creator/Game/Tasks/TransformWnd.lua
@@ -14,15 +14,13 @@ end)
 ]]
 local TransformWnd = commonlib.inherit(commonlib.gettable("System.Core.ToolBase"), commonlib.gettable("MyCompany.Aries.Game.Tasks.TransformWnd"));
 
+TransformWnd.pivot = {0,0,0};
 local groupindex_hint = 6; 
-
-------------------------
--- page function 
-------------------------
 local page;
 
 local transform = {};
 local old_transform = {};
+
 -- @param trans: {x, y, z, rot_y, blocks, method}
 -- @param callbackFunc: function(trans, result) end, where result is "ok" if user clicked ok. 
 function TransformWnd.ShowPage(blocks, trans, callbackFunc)
@@ -38,7 +36,7 @@ function TransformWnd.ShowPage(blocks, trans, callbackFunc)
 
 	old_transform = commonlib.clone(trans);
 	
-	local x, y, width, height = 124, 160, 110, 235;
+	local x, y, width, height = 124, 160, 115, 290;
 	local align = "_lt";
 	
 	local params = {
@@ -124,6 +122,22 @@ function TransformWnd.ChangeCoordinate(name,mcmlNode)
 	end
 end
 
+function TransformWnd.ChangeRotation(name)
+	if(name == "btn_sub_axis" or name == "btn_add_axis") then
+		local deltaAngle = 45;
+		local value = TransformWnd.GetNumberValue("text_rot", 0);
+		if(name == "btn_sub_axis") then
+			value = value - deltaAngle;
+		elseif(name == "btn_add_axis") then
+			value = value + deltaAngle;
+		end
+		local value = math.floor(value / deltaAngle + 0.5) * deltaAngle;
+		value = mathlib.WrapAngleTo180(value);
+		page:SetValue("text_rot", value);
+	end
+	TransformWnd.UpdateHintLocation();
+end
+
 function TransformWnd.ClosePage()
 	if(page) then
 		page:CloseWindow();
@@ -135,7 +149,7 @@ function TransformWnd.GetTransformedPoint(src_x, src_y, src_z, dx, dy, dz)
 end
 
 -- public function
-function TransformWnd.UpdateHintLocation(blocks, dx,dy,dz)
+function TransformWnd.UpdateHintLocation(blocks, dx,dy,dz, axis, rot_angle)
 	ParaTerrain.DeselectAllBlock(groupindex_hint);
 
 	blocks = blocks or TransformWnd.blocks;
@@ -150,13 +164,53 @@ function TransformWnd.UpdateHintLocation(blocks, dx,dy,dz)
 			transform.z = TransformWnd.GetNumberValue("text_pos_z", 0);
 			dx, dy, dz = transform.x, transform.y, transform.z;
 		end
+		axis = axis or page:GetValue("axis", "y");
+		rot_angle = rot_angle or (TransformWnd.GetNumberValue("text_rot", 0))*3.14/180;
 
+		local final_blocks = {};
 		if(dx~=0 or dy~=0 or dz~=0) then
-			local GetTransformedPoint = TransformWnd.GetTransformedPoint;
 			for i = 1, #blocks do
 				local b = blocks[i];
-				local x,y,z = GetTransformedPoint(b[1], b[2], b[3], dx, dy, dz);
-				ParaTerrain.SelectBlock(x,y,z, true, groupindex_hint);
+				local x,y,z = b[1]+dx, b[2]+dy, b[3]+dz;
+				final_blocks[i] = {x,y,z};
+			end
+			blocks = final_blocks;
+		end
+		if(rot_angle ~= 0) then
+			local cx, cy, cz = unpack(TransformWnd.pivot);
+			local sin_t, cos_t = math.sin(rot_angle), math.cos(rot_angle);
+
+			for i = 1, #(blocks) do
+				local b = blocks[i];
+				if(axis== "x") then
+					local x, y = b[2] - cy, b[3] - cz;
+					final_blocks[i] = {
+						b[1],
+						x*cos_t - y*sin_t + cy,
+						x*sin_t + y*cos_t + cz,
+					};
+				elseif(axis== "z") then
+					local x, y = b[1] - cx, b[2] - cy;
+					final_blocks[i] = {
+						x*cos_t - y*sin_t + cx,
+						x*sin_t + y*cos_t + cy,
+						b[3],
+					};
+				else -- if(axis== "y") then
+					local x, y = b[1] - cx, b[3] - cz;
+					final_blocks[i] = {
+						x*cos_t - y*sin_t + cx,
+						b[2],
+						x*sin_t + y*cos_t + cz,
+					};
+				end
+			end
+			blocks = final_blocks;
+		end
+		if(#final_blocks > 0) then
+			for i = 1, #final_blocks do
+				local b = final_blocks[i];
+				ParaTerrain.SelectBlock(b[1], b[2], b[3], true, groupindex_hint);
 			end
 		end
 	end
@@ -170,6 +224,9 @@ function TransformWnd.TransformSelection()
 	transform.x = TransformWnd.GetNumberValue("text_pos_x", 0);
 	transform.y = TransformWnd.GetNumberValue("text_pos_y", 0);
 	transform.z = TransformWnd.GetNumberValue("text_pos_z", 0);
+	transform.rot_angle = TransformWnd.GetNumberValue("text_rot", 0) * 3.14/180;
+	transform.rot_axis = page:GetValue("axis");
+	transform.pivot = TransformWnd.pivot;
 	transform.method = page:GetUIValue("method");
 	local scaling = page:GetValue("text_scaling", "1,1,1");
 	if(scaling) then
@@ -181,8 +238,8 @@ function TransformWnd.TransformSelection()
 		end
 	end
 	if((transform.x~=0 or transform.y~=0 or transform.z~=0) or 
-		(transform.scalingX~=1 or transform.scalingY~=1 or transform.scalingZ~=1) ) then
-
+		(transform.scalingX~=1 or transform.scalingY~=1 or transform.scalingZ~=1) or 
+		(transform.rot_axis~=0)) then
 		TransformWnd.result = "ok";
 		if(page) then
 			page:CloseWindow();
@@ -203,7 +260,6 @@ function TransformWnd.OnInit()
 	page:SetValue("text_pos_x", transform.x or 0);
 	page:SetValue("text_pos_y", transform.y or 0);
 	page:SetValue("text_pos_z", transform.z or 0);
-	page:SetValue("facing", transform.facing or 0);
 	page:SetValue("method", if_else(not transform.method or transform.method=="move" or transform.method == "no_clone", "no_clone", "clone"));
 end
 
@@ -220,3 +276,11 @@ function TransformWnd:Translate(dx, dy, dz)
 end
 
 
+function TransformWnd:Rotate(axis, angle, pivot)
+	if(page) then
+		TransformWnd.pivot = pivot or TransformWnd.pivot;
+		page:SetValue("axis", axis or "y");
+		TransformWnd.SetNumberValue("text_rot", string.format("%.2f", angle*180/3.14));
+		TransformWnd.UpdateHintLocation();
+	end
+end
